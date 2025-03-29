782a3ae4ad0ae20bba02cf89ef1f34963bdb9331
JUAN OCAMPO SALAZAR <ocampojuan035@gmail.com>
2025-03-28 23:01:05 -0500

    Comuna List

---
+++ resources/views/comuna/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <h1>Commune List</h1>
+    
+    <a href="{{ route('comunas.create') }}" class="btn btn-success">Add</a>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Code</th>
+                <th scope="col">Commune</th>
+                <th scope="col">Municipality</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comunas as $comuna)
+                <tr>
+                    <th scope="row">{{ $comuna->comu_codi }}</th>
+                    <td>{{ $comuna->comu_nomb }}</td>
+                    <td>{{ $comuna->muni_nomb }}</td>
+                    <td><span>Actions</span></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
