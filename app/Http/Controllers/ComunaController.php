7cd34e8154895938f454ba5fae718979609e8eda
JUAN OCAMPO SALAZAR <ocampojuan035@gmail.com>
2025-04-07 19:27:35 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -65,21 +65,36 @@ class ComunaController extends Controller
     public function edit(string $id)
     {
         //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+        return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
+    public function update(Request $request, $id)
+{
+    $comuna = Comuna::find($id);
+    
+    $comuna->comu_nomb = $request->name;
+    $comuna->muni_codi = $request->code;
+    $comuna->save();
+    
+    $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+    
+    return view('comuna.index', ['comunas' => $comunas]);
+}
     /**
      * Remove the specified resource from storage.
      * 
      * @param int $id
-     * @return \Illuminate/Http\Response
+     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
